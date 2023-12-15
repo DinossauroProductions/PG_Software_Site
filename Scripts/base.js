c85196927c7ecbe80948e7fe6c10df3213d8bb2c
DinossauroProductions <guilhermeovergame@gmail.com>
2023-12-15 16:04:14 -0300

    Atualizado base.js

--- Scripts/base.js
+++ Scripts/base.js
@@ -6,4 +6,24 @@ image.onload = () => {
   // Draw the image into the canvas
   ctx.drawImage(image, 0, 0);
 };
-image.src = "Imagens/index.png";
+
+var altura = 1;
+
+var pathSequence = window.location.pathname.split("/");
+
+for(var i = 0; i < pathSequence.length; i++){
+
+  if(pathSequence[i].includes("PG_Software_Site")){
+
+    var altura = pathSequence.length - (i + 1);
+    //console.log("pasta root encontrada, i = " + i + " e tamanho total = "+pathSequence.length + ", logo altura da pasta atual = "+altura);
+  }
+}
+
+var base = "";
+
+for(var i = 1; i < altura; i++){
+  base = base + "../";
+}
+
+image.src = base + "Imagens/index.png";
